5358e3d8631dc2e9e112489102f04a59e523acb1
David Zhang <crispgm@gmail.com>
2024-11-30 10:53:50 +0800

    feat: add feishu provider

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/crispgm/atsa-notifier
 
 go 1.23
+
+require github.com/go-lark/lark v1.15.0 // indirect
---
+++ internal/provider/feishu.go
@@ -0,0 +1,29 @@
+package provider
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-lark/lark"
+)
+
+var _ FeishuWebhook
+
+// FeishuWebhook .
+type FeishuWebhook struct{}
+
+// Send .
+func (fw FeishuWebhook) Send(webhookURL string, content []byte) (*http.Response, error) {
+	webhook := lark.NewNotificationBot(webhookURL)
+	msg := lark.NewMsgBuffer(lark.MsgPost).Text(string(content))
+	resp, err := webhook.PostNotificationV2(msg.Build())
+	if err != nil {
+		fmt.Println("Error sending POST request:", err)
+		return nil, err
+	}
+	if resp.Code != 0 {
+		return nil, fmt.Errorf("Lark Error: [%d] %s", resp.Code, resp.Msg)
+	}
+
+	return nil, err
+}
