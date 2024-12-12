124bdec514f33a98432d9231e9be3356462234e1
David Zhang <crispgm@gmail.com>
2024-12-12 12:35:01 +0800

    refactor(frontend): support multiple notification methods and ui polishing

--- web/js/app.js
+++ web/js/app.js
@@ -9,6 +9,7 @@ createApp({
       eventPhase: '',
       kickertoolLiveURL: '',
       discordWebhookURL: '',
+      feishuWebhookURL: '',
       locales: ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP'],
       selectedLocale: 'en-US',
 
@@ -31,9 +32,15 @@ createApp({
     window.speechSynthesis.onvoiceschanged = this.loadVoices;
   },
   methods: {
-    showError(msg) {
-      this.loadingError = msg;
-      this.log('ERROR', msg)
+    showError(...messages) {
+      const formattedMessages = messages.join(' ');
+      this.loadingError = formattedMessages;
+      this.log('ERROR', formattedMessages);
+    },
+    showWarn(...messages) {
+      const formattedMessages = messages.join(' ');
+      this.loadingError = formattedMessages;
+      this.log('WARN', formattedMessages);
     },
     log(level = 'INFO', ...messages) {
       const timestamp = new Date().toLocaleTimeString();
@@ -55,7 +62,10 @@ createApp({
     updateLocale() {
       if (this.selectedLocale) {
         for (i = 0; i < this.voices.length; i++) {
-          if (this.voices[i].name.startsWith('Google') && this.selectedLocale == this.voices[i].lang) {
+          if (
+            this.voices[i].name.startsWith('Google') &&
+            this.selectedLocale == this.voices[i].lang
+          ) {
             this.selectedVoice = this.voices[i].name;
             return;
           }
@@ -71,15 +81,14 @@ createApp({
         if (selected) {
           utterance.voice = selected;
         }
-        this.log('INFO', 'Spoke [' + text + '] with', utterance.voice.name);
+        this.log('INFO', 'Spoke [', text, '] with', utterance.voice.name);
         window.speechSynthesis.speak(utterance);
       }
     },
-    async buildMatchText(type, template, mIndex, tIndex = -1, pIndex = -1) {
+    async buildSpeakText(match, provider, template) {
       try {
         this.loadingError = 'Sending...';
         const url = '/notify';
-        const match = this.matches[mIndex];
         const params = {
           tournamentName: this.tournamentName,
           eventName: this.eventName,
@@ -88,18 +97,9 @@ createApp({
           team2: match.team2,
           tableNo: match.tableNo,
           locale: this.selectedLocale,
-          msgType: type,
+          msgType: provider,
           template: template,
         };
-        if (template == 'recall_player') {
-          let team = match.team1
-          if (tIndex == 1) {
-            team = match.team2
-          }
-          console.log(team[pIndex])
-          params.team1 = [team[pIndex]];
-          params.team2 = [];
-        }
         const response = await fetch(url, {
           method: 'POST',
           headers: {
@@ -108,7 +108,7 @@ createApp({
           body: JSON.stringify(params),
         });
         if (!response.ok) {
-          this.showError('Network response was not ok: ' + response.statusText);
+          this.showError('Network response was not ok:', response.statusText);
         }
         this.loadingError = '';
         const data = await response.json();
@@ -121,28 +121,109 @@ createApp({
       }
       return '';
     },
-    async call(index) {
-      this.textToSpeech(await this.buildMatchText('speak', 'call_match', index));
+    async notify(match, provider, template) {
+      try {
+        this.loadingError = 'Sending...';
+        const url = '/notify';
+        const params = {
+          tournamentName: this.tournamentName,
+          eventName: this.eventName,
+          eventPhase: this.eventPhase,
+          team1: match.team1,
+          team2: match.team2,
+          tableNo: match.tableNo,
+          locale: this.selectedLocale,
+          msgType: provider,
+          template: template,
+        };
+        if (provider == 'discord') {
+          params.discordWebhookURL = this.discordWebhookURL;
+        } else if (provider == 'feishu') {
+          params.feishuWebhookURL = this.feishuWebhookURL;
+        } else {
+          this.showError('Illegal provider:', provider);
+          return;
+        }
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+        if (!response.ok) {
+          this.showError('Network response was not ok:', response.statusText);
+        }
+        this.loadingError = '';
+        const data = await response.json();
+        this.log(
+          'INFO',
+          'Notified match provider: ',
+          provider,
+          'template:',
+          template,
+          ' text:',
+          data.data.text,
+        );
+      } catch (err) {
+        this.showError(err.message);
+      } finally {
+        this.loading = false;
+      }
+    },
+    async handleCall(index) {
       this.log('INFO', 'Called match index:', index);
+      const match = this.matches[index];
+      // speak
+      this.textToSpeech(
+        await this.buildSpeakText(match, 'speak', 'call_match'),
+      );
+      // web hooks
+      const template = 'call_match';
+      if (this.discordWebhookURL) {
+        await this.notify(match, 'discord', template);
+      }
+      if (this.feishuWebhookURL) {
+        await this.notify(match, 'feishu', template);
+      }
     },
-    async edit(index) {
-      this.text = await this.buildMatchText('speak', 'call_match', index);
+    async handleEdit(index) {
       this.log('INFO', 'Edited match index:', index);
+      const match = this.matches[index];
+      this.text = await this.buildSpeakText(match, 'speak', 'call_match');
     },
-    async recall(mIndex, tIndex, pIndex) {
-      this.textToSpeech(await this.buildMatchText('speak', 'recall_player', mIndex, tIndex, pIndex));
+    async handleRecall(mIndex, tIndex, pIndex) {
       this.log('INFO', 'Recalled player index:', mIndex, tIndex, pIndex);
+      const match = JSON.parse(JSON.stringify(this.matches[mIndex]));
+      let team = match.team1;
+      if (tIndex == 1) {
+        team = match.team2;
+      }
+      match.team1 = [team[pIndex]];
+      match.team2 = [];
+      // speak
+      this.textToSpeech(
+        await this.buildSpeakText(match, 'speak', 'recall_player'),
+      );
+      // web hooks
+      const template = 'recall_player';
+      if (this.discordWebhookURL) {
+        await this.notify(match, 'discord', template);
+      }
+      if (this.feishuWebhookURL) {
+        await this.notify(match, 'feishu', template);
+      }
     },
-    speakText() {
+    handleSpeakText() {
       if (this.text) {
         this.textToSpeech(this.text);
       } else {
-        this.showError('Please input texts manually.')
+        this.showWarn('Please input texts manually.');
       }
     },
-    async notifyText() {
+    async handleNotifyText() {
       if (!this.discordWebhookURL) {
-        this.showError('discordWebhookURL is not set');
+        this.showWarn('Discord Webhook URL is not set');
         return;
       }
       try {
@@ -161,7 +242,7 @@ createApp({
           body: JSON.stringify(params),
         });
         if (!response.ok) {
-          this.showError('Network response was not ok: ' + response.statusText);
+          this.showError('Network response was not ok:', response.statusText);
         }
         this.loadingError = '';
       } catch (err) {
@@ -170,49 +251,9 @@ createApp({
         this.loading = false;
       }
     },
-    async notify(index) {
-      if (!this.discordWebhookURL) {
-        this.showError('discordWebhookURL is not set');
-        return;
-      }
-      try {
-        this.loadingError = 'Sending...';
-        const url = '/notify';
-        const match = this.matches[index];
-        const params = {
-          tournamentName: this.tournamentName,
-          eventName: this.eventName,
-          eventPhase: this.eventPhase,
-          team1: match.team1,
-          team2: match.team2,
-          tableNo: match.tableNo,
-          locale: this.selectedLocale,
-          discordWebhookURL: this.discordWebhookURL,
-          msgType: 'discord',
-          template: 'call_match',
-        };
-        const response = await fetch(url, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(params),
-        });
-        if (!response.ok) {
-          this.showError('Network response was not ok: ' + response.statusText);
-        }
-        this.loadingError = '';
-        const data = await response.json();
-        this.log('INFO', 'Notified match index [', index, '] text=', data.data.text);
-      } catch (err) {
-        this.showError(err.message);
-      } finally {
-        this.loading = false;
-      }
-    },
-    async crawl() {
+    async handleCrawl() {
       if (!this.kickertoolLiveURL) {
-        this.showError('kickertoolLiveURL is not set');
+        this.showWarn('Kickertool Live URL is not set');
         return;
       }
       this.log('INFO', 'Crawled', this.kickertoolLiveURL);
@@ -221,7 +262,7 @@ createApp({
         const url = '/crawl?url=' + this.kickertoolLiveURL;
         const response = await fetch(url);
         if (!response.ok) {
-          this.showError('Network response was not ok: ' + response.statusText);
+          this.showError('Network response was not ok:', response.statusText);
         }
         const data = await response.json();
         this.matches = data.data.matches;
