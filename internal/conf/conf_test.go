9fea478e0ac1d5e0e93323ab8a1e66a1475b3307
David Zhang <crispgm@gmail.com>
2024-12-12 11:16:21 +0800

    chore: minor improvements

---
+++ internal/conf/conf_test.go
@@ -0,0 +1,23 @@
+package conf
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadConf(t *testing.T) {
+	path := "../../conf/conf.yml"
+	if os.Getenv("CI") == "true" {
+		path = "./conf/conf.yml"
+	}
+	conf, err := LoadConf(path)
+	if assert.NoError(t, err) {
+		assert.NotEmpty(t, conf.Mode)
+		assert.NotEmpty(t, conf.Port)
+		assert.NotEmpty(t, conf.ATSADB.LocalPath)
+		assert.Empty(t, conf.ATSADB.WebURL)
+		assert.NotEmpty(t, conf.Templates)
+	}
+}
